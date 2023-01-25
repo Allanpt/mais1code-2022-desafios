b4b27f639efe67aa3abaf78f5420cfb09de9a794
Allanpt <jatallantavares@gmail.com>
2023-01-24 21:06:03 -0300

    Massaborosa
    Foi feito uma abordagem inicial para o cliente

--- 1-fluxogramas/0 - Massaborosa/massaborosa.js
+++ 1-fluxogramas/0 - Massaborosa/massaborosa.js
@@ -11,21 +11,58 @@ Quero poder ver a lista de funcionários.
 Quero poder adicionar um novo funcionário a lista de funcionários.
 Quero poder remover um funcionário da lista de funcionários.
 */
-let listaDoCardapio = ["Calabresa", "Portuguesa", "Mussarela", "Frango", "Atum"]
+let listaDoCardapio = ["Calabresa", "Portuguesa", "Mussarela", "Frango", "Atum"] 
+
 let leia = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
+let opcao;
+leia.question(`Olá, o que deseja? 
+
+    1 - Ver cardápio
+    2 - Adicionar pedidos
+    3 - Adicionar novos itens
+    4 - Remover itens do cardápio
+    5 - Ver lista de pedidos
+    6 - Ver lista de funcionários
+    7 - Adicionar novo funcionário
+    8 - Remover funcionário
+    
+    `, input => {
+    
+    opcao = Number(input);
+    if(opcao == 1) {
+        console.log()
+        console.log(cardapio())
+        return leia.close()
+    } else if (opcao == 2){
+        console.log()
+        console.log(adicionarPedidos())
+        return leia.close
+    } else if (opcao == 3){
+        console.log()
+         console.log(dicionarNovosItens())
+         return leia.close
+    }
+})
 
 const cardapio = () => {
-
     return listaDoCardapio
 }
-const adicionarNovosItens = (item) => {
-    listaDoCardapio.push(item)
+
+const adicionarNovosItens = () => {
+    let item;
+    leia.question("Qual item você deseja adicionar? ", input =>{
+        item = input;
+        let itemComAPrimeiraLetraMaiuscula = `${item.charAt(0).toLowerCase}${item.substring(1)}`    
+        listaDoCardapio.push(itemComAPrimeiraLetraMaiuscula)
+        return "Item adicionado"
+    })
+    listaDoCardapio.push()
     return "Item adicionado"
-    
 }
+
 const removerItensCardapio = () => {
     
     let saborQueSeraExcluido;
@@ -42,6 +79,7 @@ const removerItensCardapio = () => {
         leia.close()
     })
 }
+
 const adicionarPedidos = () => { 
     let sabor;
     let quantidade;
@@ -52,7 +90,5 @@ const verListaDeFuncionarios = () => { }
 const adicionarNovoFuncionarioNaLista = () => { }
 const removerFuncionarioNaLista = () => { }
 
-console.log(cardapio())
-console.log(adicionarNovosItens("Bauru"))
-console.log(removerItensCardapio())
+
 
